7ecb42c237f6d859674686944a2927433967ba94
MERT <kabukcuoglumert@gmail.com>
2023-09-03 18:39:52 +0300

    video capture settings temporarily removed

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -27,14 +27,15 @@ void MainWindow::on_pushButton_clicked()
     timeType prevTimePoint;
     Mat myImage;
     namedWindow("FPS Checker");
-    VideoCapture cap(0,CAP_ANY);
-   /* cap.set(CAP_PROP_FPS, 30.0);
-    cap.set(CAP_PROP_FOURCC, VideoWriter.fourcc('m','j','p','g'));
+    VideoCapture cap(0,CAP_DSHOW);
+    //VideoCapture cap("C:\\Users\\kabuk\\OneDrive\\Resimler\\Camera Roll\\test2.mp4",CAP_ANY);
+   // cap.set(CAP_PROP_FPS, 30.0);
+   /* cap.set(CAP_PROP_FOURCC, VideoWriter.fourcc('m','j','p','g'));
     cap.set(CAP_PROP_FOURCC, VideoWriter.fourcc('M','J','P','G'));
 
     */
-    cap.set(CAP_PROP_FRAME_WIDTH, 3840); //I've maxed the resolution to get my webcam's max value
-    cap.set(CAP_PROP_FRAME_HEIGHT, 2160);
+    //cap.set(CAP_PROP_FRAME_WIDTH, 1280); //I've maxed the resolution to get my webcam's max value
+    //cap.set(CAP_PROP_FRAME_HEIGHT, 720);
 
     while (waitKey(1) != 'q')
     {
@@ -46,7 +47,6 @@ void MainWindow::on_pushButton_clicked()
            std::chrono::duration<float> duration = currentTimePoint - prevTimePoint;
            putText(myImage, "FPS:" + std::to_string(static_cast<int>(1/duration.count())),
            Point2f(0, 60), 2, 2, cv::Scalar(0,255,0),2);
-
        }
        prevTimePoint = currentTimePoint;
        String res = std::to_string(static_cast<int>(cap.get(CAP_PROP_FRAME_WIDTH))) + "x" + std::to_string(static_cast<int>(cap.get(CAP_PROP_FRAME_HEIGHT)));
